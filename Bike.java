b4755c4e8a45d364e67f58efebf4ed35cd57d5f8
venuk-sethmira <venukadrive@gmail.com>
2026-02-05 20:00:49 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,30 @@
+public class Bike extends Vehicle {
+    private int engineCapacityCC;
+
+    public Bike(String vehicleId, String brand, String model, double baseRatePerDay, boolean isAvailable, int engineCapacityCC) {
+        super(vehicleId, brand, model, baseRatePerDay, isAvailable);
+        this.engineCapacityCC = engineCapacityCC;
+    }
+
+    // Getter
+    public int getEngineCapacityCC() {
+        return engineCapacityCC;
+    }
+
+    //Setter
+    public void setEngineCapacityCC(int engineCapacityCC) {
+        this.engineCapacityCC = engineCapacityCC;
+    }
+
+    //Rental cost calculation
+    @Override
+    public double calculateRentalCost(int days) {
+        return (getBaseRatePerDay() * days) + (engineCapacityCC * 0.5 * days);
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Engine Capacity: " + engineCapacityCC + "cc");
+    }
+}
